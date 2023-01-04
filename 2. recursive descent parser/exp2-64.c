acfef464f4ed6c2efb6a97507d816df090004c02
Navaneeth Venu <nauaneeth@gmail.com>
2023-01-04 23:32:49 +0530

    Update Functions

--- 2. recursive descent parser/exp2-64.c
+++ 2. recursive descent parser/exp2-64.c
@@ -1,32 +1,90 @@
 // 2. Recursive descent parser
-// Amal Joe R S, Roll No. 64, S7 CS2
+// Navaneeth Venu, Roll No. 44, S7 CS2
 
 #include <stdio.h>
 #define MAX 1000
 
 char s[MAX];
-char* p = s;
+char *p = s;
 
 int E();
 int T();
 int F();
 
-int main() {
+int main()
+{
 	printf("Enter a string:\n");
 	scanf("%s", s);
-	if(E() & *p == '\0')
+	if (E() & *p == '\0')
 		printf("Successfully parsed\n");
 	else
 		printf("Error in parsing\n");
 	return 0;
 }
 
-int E() {
-	if(*p == '+') {
+// int E()
+// {
+// 	if (*p == '+')
+// 	{
+// 		p++;
+// 		if (T())
+// 		{
+// 			if (F())
+// 			{
+// 				printf("E -> +TF\n");
+// 				return 1;
+// 			}
+// 		}
+// 	}
+// 	return 0;
+// }
+
+// int T()
+// {
+// 	if (*p == 'a')
+// 	{
+// 		p++;
+// 		if (*p == 'b')
+// 		{
+// 			printf("T -> ab\n");
+// 			p++;
+// 			return 1;
+// 		}
+// 		printf("T -> a\n");
+// 		return 1;
+// 	}
+// 	return 0;
+// }
+
+// int F()
+// {
+// 	if (T())
+// 	{
+// 		if (*p == 'a')
+// 		{
+// 			p++;
+// 			if (F())
+// 			{
+// 				printf("F -> TaF\n");
+// 				return 1;
+// 			}
+// 		}
+// 		return 0;
+// 	}
+// 	printf("F -> $\n");
+// 	return 1;
+// }
+
+int E()
+{
+	if (*p == '+')
+	{
 		p++;
-		if(T()) {
-			if(F()) {
-				printf("E -> +TF\n");
+		if (T())
+		{
+			if (F())
+			{
+				printf("E->+TF\n");
 				return 1;
 			}
 		}
@@ -34,33 +92,39 @@ int E() {
 	return 0;
 }
 
-int T() {
-	if(*p == 'a') {
-		p++;
-		if (*p == 'b') {
-			printf("T -> ab\n");
+int F()
+{
+	if (T())
+	{
+		if (*p == 'a')
+		{
 			p++;
+			if (F())
+			{
+				printf("E->TaF\n");
+				return 1;
+			}
+		}
+	}
+	printf("E->$\n");
+	return 1;
+}
+
+int T()
+{
+	if (*p == 'a')
+	{
+		p++;
+		if (*p == 'b')
+		{
+			p++;
+			printf("T->ab\n");
 			return 1;
 		}
-		printf("T -> a\n");
+		printf("T->a\n");
 		return 1;
 	}
 	return 0;
-}
-
-int F() {
-	if(T()) {
-		if(*p == 'a') {
-			p++;
-			if(F()) {
-					printf("F -> TaF\n");
-					return 1;
-				}
-		}
-		return 0;
-	}
-	printf("F -> $\n");
-	return 1;
 }
 
 /*
@@ -87,4 +151,3 @@ Enter a string:
 +ba
 Error in parsing
 */
-
